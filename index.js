4f659c35df8c5b401d6a25d05d0180e7db6c0172
Alexander Whan <whan.alexander@gmail.com>
2020-08-10 22:16:59 -0700

    Set up structure for connecting input/notes files and logging input object

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// requires library files
+const Input = require('./lib/input.js');
+// const Notes = require('./lib/notes.js');
+
+// instantiates an instance of Input module and initializes in constant 'options'
+const options = new Input();
+console.log(options);
+
+// Passes the command/options to Notes library to execute
+// Notes.send(options);
