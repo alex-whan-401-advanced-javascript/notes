8bb87de051f83ea7ef521e7d08f4a0d9cd839e37
Alexander Whan <whan.alexander@gmail.com>
2020-08-13 20:21:24 -0700

    Tweaked Notes testing for saved notes

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('@code-fellows/supergoose');
+
 const Notes = require('../lib/notes.js');
 
 let notes = new Notes();
@@ -17,5 +19,14 @@ describe('Notes Module', () => {
     const payload = 'test note';
     const note = notes.execute({ action, payload });
     expect(notes.add).toHaveBeenCalled();
+  });
+
+  it('can display a saved note', () => {
+    const action = 'add';
+    const payload = 'test note';
+    return notes.execute({ action, payload }).then((results) => {
+      expect(results.category).toBe('general');
+      expect(results.payload).toBe('test note');
+    });
   });
 });
