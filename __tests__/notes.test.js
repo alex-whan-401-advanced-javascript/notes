d99235b8f4bf018a0e40a97b723d3a3823dc24d2
Alexander Whan <whan.alexander@gmail.com>
2020-08-13 22:58:56 -0700

    Added list test to Notes module test file

--- __tests__/notes.test.js
+++ __tests__/notes.test.js
@@ -5,7 +5,7 @@ require('@code-fellows/supergoose');
 const Notes = require('../lib/notes.js');
 
 let notes = new Notes();
-jest.spyOn(notes, 'add');
+let spy = jest.spyOn(notes, 'add');
 
 describe('Notes Module', () => {
   it('nothing logged to the console if no valid options given', () => {
@@ -21,12 +21,20 @@ describe('Notes Module', () => {
     expect(notes.add).toHaveBeenCalled();
   });
 
-  it('can display a saved note', () => {
+  it.skip('can display a saved note', () => {
     const action = 'add';
     const payload = 'test note';
     return notes.execute({ action, payload }).then((results) => {
       expect(results.category).toBe('general');
       expect(results.payload).toBe('test note');
     });
+  });
+
+  it('should be able to list stored notes from the database', async () => {
+    const action = 'add';
+    const payload = 'testing list';
+    await notes.execute({ action, payload });
+    await notes.execute({ action: 'list' });
+    expect(spy).toHaveBeenCalled();
   });
 });
